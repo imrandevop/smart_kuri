7d363a904eb1d725ec3476d39c4b1face2ea0865
imrandevop <imranprvt11@gmail.com>
2025-11-17 14:50:13 +0530

    added allowed host

--- smart_kuri/settings.py
+++ smart_kuri/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-rvtmadnkoybg$3v7+#p%v0m@d0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
