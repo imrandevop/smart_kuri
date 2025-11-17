ad6b815f0454ec45b433cab8a5cdc0abbc98ed58
imrandevop <imranprvt11@gmail.com>
2025-11-17 14:44:14 +0530

    added static file

--- smart_kuri/settings.py
+++ smart_kuri/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
+
+MEDIA_URL = '/media/'  # Add this line too
+MEDIA_ROOT = BASE_DIR / 'media'  # Add this line too
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
